c73823da127aec271dd2870613ed6edb45b92ec8
Arifuzzaman Pranto <arifuzzamanpranto@ymail.com>
2018-05-16 18:20:44 +0600

    Template Widget Add System Update -Added option in widget property

--- app/Admin/Controllers/Website/TemplatesMetaController.php
+++ app/Admin/Controllers/Website/TemplatesMetaController.php
@@ -141,7 +141,7 @@ class TemplatesMetaController extends Controller
             $form->select('parent_id','Parent Widget')->options(SiteTemplatesMeta::selectedOptions2($this->tid));
             $form->select('widgets_id','Widget')->options(Widgets::selectOptions())->default(key(Widgets::selectOptions()));
             $form->hasMany('templates_meta_values','Widget Properties',function (Form\NestedForm $form){
-                $form->select('meta_key','Property Name')->options(array('Height','Width'));
+                $form->select('meta_key','Property Name')->options(array('style'=>'Style','innserstyle'=>'InnerStyle','class'=>'Class','innerclass'=>'InnerClass'));
                 $form->text('meta_value','Value');
             });
 
